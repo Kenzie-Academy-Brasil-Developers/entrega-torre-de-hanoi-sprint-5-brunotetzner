f45d7ae672181d094b94ff7bd274f87fe7dcf002
Bruno Tetzner <brunoltetzner@gmail.com>
2021-09-27 15:57:51 -0300

    adicionando as torres e blocos via script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const container = document.getElementById("container")
+
+//divs que armazenam os blocos
+const tower1 = document.createElement("div")
+const tower2 = document.createElement("div")
+const tower3 = document.createElement("div")
+
+container.appendChild(tower1)
+container.appendChild(tower2)
+container.appendChild(tower3)
+
+//blocos
+const bloco1 = document.createElement("div")
+const bloco2 = document.createElement("div")
+const bloco3 = document.createElement("div")
+const bloco4 = document.createElement("div")
+
+tower1.appendChild(bloco1)
+tower1.appendChild(bloco2)
+tower1.appendChild(bloco3)
+tower1.appendChild(bloco4)
+
+
